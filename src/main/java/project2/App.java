8547faf8036dac70f0347a281f51ccf7240e3d78
phu.story <konthainakab@gmail.com>
2025-06-16 10:45:38 +0700

    init commit

---
+++ src/main/java/project2/App.java
@@ -0,0 +1,16 @@
+package project2;
+
+/* 
+   Made with ♥ by,
+
+   Yoswaris Lawpaiboon,  6681170
+   Pasin Piyavej,        6681187
+   Praepilai Phetsamsri, 6681374
+   
+*/
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
